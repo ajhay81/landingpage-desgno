9b9e70ca69d0bec692203827783789dcd39e7a66
Arie Sby <82284895+ajhay81@users.noreply.github.com>
2023-10-31 12:12:53 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# landingpage-desgno
-Simple Landing Page
+This is the first time I create website by my self with html, css and javaScript.
+This project or This project or exercise project was originally designed for use with astro.build, but as a beginner I find it is a As a beginner, I feel like I don't understand and master astro.build, and even now I'm still a beginner in the field of JavaScript (2023, 31th October).
+
+So I decided to make this landing page with basic code in HTML, CSS and JavaScript, and hope one day I can recreate this page full in astro.build or better JavaScript.
+
+I create this website within 2 weeks, LOL, but I gain a lot of experience, and it is true when @Thomas Weibenfalk says "If you want to code, you have to type it, not copy paste, because it is the way you memorize the code'.
+
+If you want to watch the full video of the lesson, here is the link https://www.youtube.com/watch?v=jyjScZWgzIg&list=PL4cUxeGkcC9hZm9NYpd4G-jhoeEk0ls--
+
+or
+
+https://youtu.be/jyjScZWgzIg?si=jFNPdSBpXfiT-BlU
+
+and
+
+Make sure you subscribe this channel https://www.youtube.com/c/codinginpublic
---
+++ main.js
@@ -0,0 +1,97 @@
+// local reviews data
+const reviews = [
+  {
+    id: 0,
+    author: 'Nobert Noob',
+    job: 'Xiao Mi',
+    img: './images/nobert-noob.png',
+    comment: "Lorem ipsum dolor sit amet.",
+  },
+  {
+    id: 1,
+    author: 'Susan Smith',
+    job: 'CNN',
+    img: './images/susan-smith.png',
+    comment: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up.",
+  },
+  {
+    id: 2,
+    author: 'Anna Johnson',
+    company: 'Adobe',
+    img: './images/anna-johnson.png',
+    comment: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle.',
+  },
+  {
+    id: 3,
+    author: 'Peter Jones',
+    company: 'Intern',
+    img: './images/peter-jones.png',
+    comment: 'Sriracha literally flexitarian irony, vape marfa unicorn.',
+  },
+  {
+    id: 4,
+    author: 'Bill Anderson',
+    company: 'Gog.com',
+    img: './images/bill-anderson.png',
+    comment: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually.',
+  },
+];
+
+// select items
+const img = document.getElementById ("person-img");
+const comment = document.getElementById ("comment");
+const author = document.getElementById ("author");
+const company = document.getElementById ("company");
+
+const prevBtn = document.querySelector (".prev-btn");
+const nextBtn = document.querySelector (".next-btn");
+
+// set awal mula item
+let currentItem = 0;
+
+//load initial item 
+
+window.addEventListener('DOMContentLoaded', function () {
+  //console.log ("shake dan shack"); testing tampil di browser
+  /*const item = reviews [currentItem];
+  img.src = item.img;
+  author.textContent = item.author;
+  company.textContent = item.company;
+  comment.textContent = item.comment; */
+
+  //dipanggil dari function showPerson
+
+  showPerson(currentItem);
+});
+
+//setelah ke-4 item ini dibuat kita buat function yang berisi ke-4 item diatas, 
+//jadi nanti yang diatas adalah pemanggilan funciton saja
+
+function showPerson(testimonial) {
+  const item = reviews [testimonial];
+  img.src = item.img;
+  author.textContent = item.author;
+  company.textContent = item.company;
+  comment.textContent = item.comment;  
+};
+
+
+// next person
+nextBtn = addEventListener('click', function () {
+  currentItem++;
+  //mengakses bagian terakhir dari array reviews, jadi current item kembali ke array 0//
+  if (currentItem > reviews.length -1) { 
+    currentItem = 0;
+  }
+  showPerson(currentItem);  
+});
+
+// prev person
+prevBtnBtn = addEventListener('click', function () {
+  currentItem--;
+  //mengakses bagian terakhir dari array reviews, jadi current item kembali ke array 0//
+  if (currentItem < reviews.length -1) { 
+    currentItem = 0;
+  }
+  showPerson(currentItem);  
+});
